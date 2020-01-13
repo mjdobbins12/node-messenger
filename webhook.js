01b322fd1c17ec323618ea3010c4d0a4281ee049
Mark Dobbins <mjdobbins12@gmail.com>
2020-01-13 14:53:22 +0000

    some more setup

---
+++ webhook.js
@@ -0,0 +1,34 @@
+/* global sendMessage*/
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 5000, () => {
+	console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+app.get('/webhook', (req, res) => {
+	if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		res.status(403).end();
+	}	
+});
+
+app.post('/webhook', (req, res) => {
+	console.log(req.body);
+	if (req.body.object === 'page') {
+		req.body.entry.forEach((entry) => {
+			entry.messaging.forEach((event) => {
+				if (event.message && event.message.text) {
+					sendMessage(event);
+				}
+			});
+		});
+		res.status(200).end();
+	}
+});
